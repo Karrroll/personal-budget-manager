69e121980df317693672acae52ed0102a3378fa5
Karol Jurzec <karol.jurzec.programista@gmail.com>
2026-01-18 16:20:50 +0100

    Add JS to show/hide custom date picker

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// sidebar menu action
 const sidebarMenu = document.querySelector(".sidebar-nav-menu");
 
 function showSidebar() {
@@ -7,3 +8,17 @@ function showSidebar() {
 function hideSidebar() {
   sidebarMenu.classList.remove('active');
 }
+
+// custom date actions
+const selectPeriod = document.querySelector("#select-period");
+const customDatesPicker = document.querySelector("#select-custom-dates");
+
+selectPeriod.addEventListener("change", () => {
+  if(selectPeriod.value === "4") {
+    customDatesPicker.hidden = false;
+    selectPeriod.setAttribute("aria-expanded", "true");
+  } else {
+    customDatesPicker.hidden = true;
+    selectPeriod.setAttribute("aria-expanded", "false");
+  }
+});
